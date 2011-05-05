54221add587fffbfad303ba02d5c7b2b2af6190d
Burning Daylight <dmitry.v.potapov@gmail.com>
2011-05-06 03:56:42 +0400

    test: added more complex case + test.sh

---
+++ test/test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ROOT="${BASH_SOURCE[0]}";
+if([ -h "${ROOT}" ]) then
+  while([ -h "${ROOT}" ]) do ROOT=`readlink "${ROOT}"`; done
+fi
+ROOT=$(cd `dirname "${ROOT}"` && pwd)
+
+pushd "${ROOT}" >/dev/null
+
+echo "--> running ad-hoc tests"
+
+for f in data/*.json; do
+
+  f=${f%.json}
+  f=${f#json/}
+
+  ERROR_MSG=`../json2lua -rN <${f}.json 2>&1 | grep "stack traceback:" -B1 -A100`
+
+  if [[ $ERROR_MSG ]]; then
+    echo "${f}.json: failed:"
+    ../json2lua -rN <${f}.json 2>&1
+    continue
+  else
+    echo "${f}.json: OK"
+  fi
+
+done
+
+popd >/dev/null
+
